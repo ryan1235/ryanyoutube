ae601a9a31936df49cb323451ceb5ac2857b2561
ryan1235 <58185393+ryan1235@users.noreply.github.com>
2022-01-06 18:54:40 -0300

    :warning: novas animações

--- js/app.js
+++ js/app.js
@@ -1,9 +1,49 @@
-var i = setInterval(function () {
-    
-    clearInterval(i);
-  
-    // O código desejado é apenas isto:
-    document.getElementById("pre-carregamento").style.display = "none";
-    document.getElementById("teste").style.display = "inline";
+$('.menu #roll').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href');
+        targetOffset = $(id).offset().top;
+    $('html, boby').animate({
+        scrollTop: targetOffset
+    }, 1000);
+});
 
-}, 4000);
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+
+(function(){
+	var $target = $('.videos-todos'),
+			animationClass = 'anime-start',
+			offset = $(window).height() * 3/4;
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+
+		$target.each(function(){
+			var itemTop = $(this).offset().top;
+			if (documentTop > itemTop - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		});
+	}
+
+	animeScroll();
+
+	$(document).scroll(debounce(function(){
+		animeScroll();
+	}, 200));
+})();
